d4bfa9594ba788f0006d2a74ace351ea04e8474d
Sathyasree-Sundaravel <50878140+Sathyasree-Sundaravel@users.noreply.github.com>
2022-07-25 10:12:53 +0530

    Create  Write a code to get the input in the given format and print the output in the given format.py

---
+++ Write a code to get the input in the given format and print the output in the given format.py
@@ -0,0 +1,26 @@
+"""
+Write a code to get the input in the given format and print the output in the given format
+
+Input Description:
+First-line indicates two integers separated by space. Second-line indicates two integers separated by space. Third-line indicates two integers separated by space.
+
+Output Description:
+Print the input in the same format.
+
+Sample Input :
+2 4
+2 4
+2 4
+Sample Output :
+2 4
+2 4
+2 4
+"""
+
+
+L1=list(map(int,input().split()))
+L2=list(map(int,input().split()))
+L3=list(map(int,input().split()))
+print(*L1,sep=' ')
+print(*L2,sep=' ')
+print(*L3,sep=' ')
