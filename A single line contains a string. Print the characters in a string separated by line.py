a517c9c76bb70a7e4d6b3e4a21858b07d611ea9c
Sathyasree-Sundaravel <50878140+Sathyasree-Sundaravel@users.noreply.github.com>
2022-07-25 11:13:28 +0530

    Create A single line contains a string. Print the characters in a string separated by line.py

---
+++ A single line contains a string. Print the characters in a string separated by line.py
@@ -0,0 +1,26 @@
+"""
+Write a code to get the input in the given format and print the output in the given format.
+
+Input Description:
+A single line contains a string.
+
+Output Description:
+Print the characters in a string separated by line.
+
+Sample Input :
+guvigeek
+Sample Output :
+g
+u
+v
+i
+g
+e
+e
+k
+"""
+
+
+a=str(input())
+for i in range(len(a)):
+    print(a[i])
